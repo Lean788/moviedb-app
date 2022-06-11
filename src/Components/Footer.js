5af2899f794bdd9ce56db74bedfafa10a7efdc1a
Leandro S <76525924+Lean788@users.noreply.github.com>
2022-06-11 17:42:00 +0200

    First commit - initial configuration

---
+++ src/Components/Footer.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <div className="row">
+            <div className="col-sm-12">
+                <div className="footer p-3 mt-4 text-center bg-dark text-light">
+                    Developed by: 
+                    <span className="text-warning font-weight-normal">
+                        Leandro Salvado
+                    </span>
+                    , using <i className="fab fa-react" /> React JS &amp; redux JS integrated with external movie data API (MovieDB)
+                </div>
+            </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Footer
